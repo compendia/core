882081cd7087f2169afb7ba45ba52ca0bd2cbab7
Dean van Dugteren <hello@dean.press>
2019-09-17 14:16:33 +0200

    revert last commit

--- plugins/storage/src/storage.ts
+++ plugins/storage/src/storage.ts
@@ -30,30 +30,7 @@ export const plugin: Container.IPluginDescriptor = {
             entities: [Stake, Statistic, Round],
             synchronize: true,
         });
-        startServer({
-            host: "0.0.0.0",
-            port: 8000,
-            whitelist: ["*"],
-            rateLimit: {
-                enabled: !process.env.CORE_API_RATE_LIMIT,
-                pathLimit: false,
-                userLimit: process.env.CORE_API_RATE_LIMIT_USER_LIMIT || 300,
-                userCache: {
-                    expiresIn: process.env.CORE_API_RATE_LIMIT_USER_EXPIRES || 60000,
-                },
-            },
-            cache: {
-                enabled: true,
-                generateTimeout: process.env.CORE_API_CACHE_TIMEOUT || 8000,
-            },
-            ssl: {
-                enabled: process.env.CORE_API_SSL,
-                host: process.env.CORE_API_SSL_HOST || "0.0.0.0",
-                port: process.env.CORE_API_SSL_PORT || 8443,
-                key: process.env.CORE_API_SSL_KEY,
-                cert: process.env.CORE_API_SSL_CERT,
-            },
-        });
+        startServer({ host: "0.0.0.0", port: 8000 });
     },
     async deregister(container: Container.IContainer, options) {
         logger.info(`Deregistering Storage Plug-in.`);
