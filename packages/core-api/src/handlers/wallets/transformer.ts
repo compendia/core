ffcbf9b3074cedc4b7703b4886a64ec2d90816dd
Dean van Dugteren <hello@dean.press>
2019-09-21 22:09:31 +0200

    chore(api): convert stake timestamps to unix

--- packages/core-api/src/handlers/wallets/transformer.ts
+++ packages/core-api/src/handlers/wallets/transformer.ts
@@ -1,6 +1,14 @@
+import { formatTimestamp } from "@arkecosystem/core-utils";
 import { Utils } from "@nosplatform/crypto";
 
 export const transformWallet = model => {
+    for (const key of Object.keys(model.stake)) {
+        const newKey = formatTimestamp(Number(key)).unix.toString();
+        model.stake[newKey] = model.stake[key];
+        model.stake[newKey].redeemableTimestamp = formatTimestamp(model.stake[key].redeemableTimestamp).unix;
+        delete model.stake[key];
+    }
+
     return {
         address: model.address,
         publicKey: model.publicKey,
