14de0211769207769e81488d9d32717cc4d9b09b
deanpress <hello@dean.press>
2020-09-24 14:22:16 +0200

    chore: remove magistrate unit tests

--- __tests__/unit/core-magistrate/handlers/__mocks__/transaction-reader.ts
+++
@@ -1,4 +0,0 @@
-export const transactionReader = {
-    // tslint:disable-next-line:no-empty
-    read: () => {},
-};
--- __tests__/unit/core-magistrate/handlers/__mocks__/wallet-repository.ts
+++
@@ -1,7 +0,0 @@
-export const walletRepository = {
-    // tslint:disable-next-line:no-empty
-    findByPublicKey: publicKey => {},
-    // tslint:disable-next-line:no-empty
-    index: () => {},
-    getIndex: () => ({ values: () => [] }),
-};
