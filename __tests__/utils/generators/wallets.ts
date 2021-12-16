cced1ee75a814f3cd5476616444b388296117b22
deanpress <hello@dean.press>
2021-12-16 17:30:10 +0100

    chore(networks)

--- __tests__/utils/generators/wallets.ts
+++ __tests__/utils/generators/wallets.ts
@@ -3,7 +3,11 @@ import { generateMnemonic } from "bip39";
 
 export const generateWallets = (network, quantity = 10) => {
     network = network || "testnet";
-    if (!["testnet", "mainnet", "devnet", "unitnet", "nospluginnet", "nosdevnet", "realtestnet"].includes(network)) {
+    if (
+        !["testnet", "mainnet", "devnet", "unitnet", "nospluginnet", "nosdevnet", "realtestnet", "staging"].includes(
+            network,
+        )
+    ) {
         throw new Error("Invalid network");
     }
 
